25c34f80dc724531640cd103c5c244aedb98516a
Dean Reilly <dean.reilly@gmail.com>
2012-11-18 23:29:21 +0000

    Adding missing changes from previous commits.

--- server.js
+++ server.js
@@ -6,46 +6,47 @@ var util = require('util'),
     Db = mongo.Db,
     http = require('http');
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-var db = new Db('twitter_consignments', server);
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/';
 
 var twit = new twitter();
 
-db.open(function(err, db) {
-    if(!err) {
-        db.collection('codes', function(err, collection) {
-            new cronJob('* * * * * 0', function() {
-                twit.search('@royalmail', function(data) {
-                    if (data.results) {
-                        //console.log(data);
-                        data.results.forEach(function(tweet) {
-                            var regex = /([A-Z]{2}[0-9]{9}[A-Z]{2})/ig;
-                            tweet.text.match(regex).forEach(function(tracking_number) {
-                                var now = new Date();
-                                collection.findOne({code: tracking_number}, function(err, item) {
-                                    if (!err && !item) {
-                                        collection.insert([{'code': tracking_number, 'date': now}], {safe:false}, function(err, result) {});
-                                    }
-                                });
+mongo.Db.connect(mongoUri, function (err, db) {
+    db.collection('codes', function (err, collection) {
+        new cronJob('* * * * * 0', function () {
+            twit.search('@royalmail', function (data) {
+                if (data.results) {
+                    //console.log(data);
+                    data.results.forEach(function (tweet) {
+                        var regex = /([A-Z]{2}[0-9]{9}[A-Z]{2})/ig;
+                        tweet.text.match(regex).forEach(function(tracking_number) {
+                            var now = new Date();
+                            collection.findOne({code: tracking_number}, function(err, item) {
+                                if (!err && !item) {
+                                    collection.insert([{'code': tracking_number, 'date': now}], {safe:false}, function(err, result) {});
+                                }
                             });
                         });
-                    }
-                });
-            }, null, true, "America/Los_Angeles");
+                    });
+                }
+            });
+        }, null, true, "America/Los_Angeles");
 
-            http.createServer(function (req, res) {
-                collection.find().toArray(function(err, items) {
-                    if (!err && items) {
-                        res.writeHead(200, {'Content-Type': 'text/plain'});
-                        items.forEach(function(item) {
-                            res.write(item.code + "\r\n");
-                        });
-                        res.end();
-                    }
-                });
-            }).listen(1337, '192.168.56.101');
+        var port = process.env.PORT || 5000;
 
-            console.log('Server running at http://127.0.0.1:1337/');
-        });
-    }
+        http.createServer(function (req, res) {
+            collection.find().toArray(function(err, items) {
+                if (!err && items) {
+                    res.writeHead(200, {'Content-Type': 'text/plain'});
+                    items.forEach(function(item) {
+                        res.write(item.code + "\r\n");
+                    });
+                    res.end();
+                }
+            });
+        }).listen(port);
+
+        console.log('Server running at http://127.0.0.1:' + port + '/');
+    });
 });
